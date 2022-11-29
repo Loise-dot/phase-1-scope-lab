34a7cf0d1bc158e9397095b18c8269f764a74f9a
Loise <loise.wambui@student.moringaschool.com>
2022-11-29 08:25:53 +0300

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+//console.log(upperCaseCustomerName());
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+//console.log(setBestCustomer())
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+//console.log(overwriteBestCustomer())
+const leastFavoriteCustomer = "Mary";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "John";
+}
+//console.log(changeLeastFavoriteCustomer())
+
+// function upperCaseCustomerName() {
+//   customerName.toUpperCase();
+// }
+
+// const bestCustomer = setBestCustomer() = "not bob";
+
+// function overwriteBestCustomer() {
+//   bestCustomer = "bob";
+// }
+
+// const leastFavoriteCustomer = "lo";
+// function changeLeastFavoriteCustomer() {
+//   leastFavoriteCustomer = "ise";
+// }
